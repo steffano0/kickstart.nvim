797b480add318dddd75d48b97b9f342e8da687c7
Steffano <steffanouromero@gmail.com>
2025-09-07 13:15:24 -0500

    Add type annotations for the lsp function

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -41,8 +41,11 @@ return {
           map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
           map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
 
-          -- Document highlighting
-          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+          ---@param client vim.lsp.Client
+          ---@param method vim.lsp.protocol.Method
+          ---@param bufnr? integer some lsp support methods only in specific files
+          ---@return boolean
           local function client_supports_method(client, method, bufnr)
             if vim.fn.has 'nvim-0.11' == 1 then
               return client:supports_method(method, bufnr)
@@ -50,6 +53,9 @@ return {
               return client.supports_method(method, { bufnr = bufnr })
             end
           end
+
+          -- Document highlighting
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
 
           if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
