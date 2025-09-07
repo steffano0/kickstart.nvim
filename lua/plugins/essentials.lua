e20cf8bf6205cef83fc6c0db7f9d36d6b001c856
Steffano <steffanouromero@gmail.com>
2025-09-07 12:04:23 -0500

    Refactor the configuration to improve order and understanding

---
+++ lua/plugins/essentials.lua
@@ -0,0 +1,29 @@
+return {
+  -- Detect tabstop and shiftwidth automatically
+  'NMAC427/guess-indent.nvim',
+
+  -- Lua functions that many plugins use
+  'nvim-lua/plenary.nvim',
+
+  -- Git signs
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+
+  -- Todo comments
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = false },
+  },
+}
---
+++ lua/plugins/mini.lua
@@ -0,0 +1,19 @@
+return {
+  'echasnovski/mini.nvim',
+  config = function()
+    -- Better Around/Inside textobjects
+    require('mini.ai').setup { n_lines = 500 }
+
+    -- Add/delete/replace surroundings
+    require('mini.surround').setup()
+
+    -- Simple statusline
+    local statusline = require 'mini.statusline'
+    statusline.setup { use_icons = vim.g.have_nerd_font }
+
+    ---@diagnostic disable-next-line: duplicate-set-field
+    statusline.section_location = function()
+      return '%2l:%-2v'
+    end
+  end,
+}
